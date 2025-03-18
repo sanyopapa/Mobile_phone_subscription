1747156feeb124130207a9502e39080ea0e1ffdd
sanyopapa <sandormarton265@gmail.com>
2025-03-18 19:59:51 +0100

    register activity hozzaadasa

--- app/src/main/java/com/example/mobile_phone_subscription/MainActivity.java
+++ app/src/main/java/com/example/mobile_phone_subscription/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mobile_phone_subscription;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -11,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+    private static final int SECRET_KEY = 99;
+    EditText userNameET;
+    EditText passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +26,25 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            userNameET = findViewById(R.id.editTextUsername);
+            passwordET = findViewById(R.id.editTextPassword);
             return insets;
         });
     }
 
     public void login(View view) {
-        EditText userName = findViewById(R.id.editTextUsername);
-        EditText password = findViewById(R.id.editTextPassword);
-        EditText passwordConfirm = findViewById(R.id.editTextPasswordAgain);
+
+
+        String username = userNameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG, "Username: " + username + " Password: " + password);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, Register.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
     }
 }
