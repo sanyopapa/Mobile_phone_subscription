ba87d6b3c8f68ec6ceda007dd5a7114263e23954
sanyopapa <sandormarton265@gmail.com>
2025-03-19 14:11:06 +0100

    register es login design kesz

--- app/src/main/java/com/example/mobile_phone_subscription/MainActivity.java
+++ app/src/main/java/com/example/mobile_phone_subscription/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mobile_phone_subscription;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String PEF_KEY = MainActivity.class.getPackage().toString();
+    private SharedPreferences preferences;
     private static final int SECRET_KEY = 99;
     EditText userNameET;
     EditText passwordET;
@@ -29,20 +32,60 @@ public class MainActivity extends AppCompatActivity {
 
             userNameET = findViewById(R.id.editTextUsername);
             passwordET = findViewById(R.id.editTextPassword);
+
+            preferences = getSharedPreferences(PEF_KEY, MODE_PRIVATE);
             return insets;
         });
     }
 
-    public void login(View view) {
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("username", userNameET.getText().toString());
+        editor.putString("password", passwordET.getText().toString());
+        editor.apply();
+        Log.i(LOG_TAG, "onPause");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
+    }
+
+    public void login(View view){
         String username = userNameET.getText().toString();
         String password = passwordET.getText().toString();
 
         Log.i(LOG_TAG, "Username: " + username + " Password: " + password);
     }
 
-    public void register(View view) {
+    public void toRegister(View view) {
         Intent intent = new Intent(this, Register.class);
         intent.putExtra("SECRET_KEY", SECRET_KEY);
         startActivity(intent);
--- app/src/main/java/com/example/mobile_phone_subscription/Register.java
+++ app/src/main/java/com/example/mobile_phone_subscription/Register.java
@@ -1,26 +1,112 @@
 package com.example.mobile_phone_subscription;
 
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+        import androidx.activity.EdgeToEdge;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.core.graphics.Insets;
+        import androidx.core.view.ViewCompat;
+        import androidx.core.view.WindowInsetsCompat;
 
-public class Register extends AppCompatActivity {
+        public class Register extends AppCompatActivity {
+            private static final String LOG_TAG = Register.class.getName();
+            private static final String PEF_KEY = MainActivity.class.getPackage().toString();
+            private SharedPreferences preferences;
+            EditText newUsernameET;
+            EditText passwordET;
+            EditText passwordAgainET;
+            EditText emailET;
+            EditText phoneET;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                EdgeToEdge.enable(this);
+                setContentView(R.layout.activity_register);
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-    }
-}
+                    newUsernameET = findViewById(R.id.editTextNewUsername);
+                    passwordET = findViewById(R.id.editTextPassword);
+                    passwordAgainET = findViewById(R.id.editTextPasswordAgain);
+                    emailET = findViewById(R.id.editTextTextEmailAddress);
+                    phoneET = findViewById(R.id.editTextPhone);
+
+                    preferences = getSharedPreferences(PEF_KEY, MODE_PRIVATE);
+                    String username = preferences.getString("username", "");
+                    String password = preferences.getString("password", "");
+                    newUsernameET.setText(username);
+                    passwordET.setText(password);
+                    passwordAgainET.setText(password);
+                    return insets;
+                });
+                int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
+
+                if (secret_key != 99) {
+                    finish();
+                }
+            }
+
+            @Override
+            protected void onPause() {
+                super.onPause();
+                Log.i(LOG_TAG, "onPause");
+            }
+
+            @Override
+            protected void onResume() {
+                super.onResume();
+                Log.i(LOG_TAG, "onResume");
+            }
+
+            @Override
+            protected void onDestroy() {
+                super.onDestroy();
+                Log.i(LOG_TAG, "onDestroy");
+            }
+
+            @Override
+            protected void onStart() {
+                super.onStart();
+                Log.i(LOG_TAG, "onStart");
+            }
+
+            @Override
+            protected void onStop() {
+                super.onStop();
+                Log.i(LOG_TAG, "onStop");
+            }
+
+            @Override
+            protected void onRestart() {
+                super.onRestart();
+                Log.i(LOG_TAG, "onRestart");
+            }
+
+            public void register(View view) {
+                String newUsername = newUsernameET.getText().toString();
+                String password = passwordET.getText().toString();
+                String passwordAgain = passwordAgainET.getText().toString();
+                String email = emailET.getText().toString();
+                String phone = phoneET.getText().toString();
+
+                if (!password.equals(passwordAgain)) {
+                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Log.i(LOG_TAG, "New Username: " + newUsername + " Password: " + password + " Password Again: " + passwordAgain + " Email: " + email + " Phone: " + phone);
+            }
+
+            public void back(View view) {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
